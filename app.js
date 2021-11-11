2977d2d9061393c2b8b137a6a6af9bbada7259c6
Christian Johannesson <christian.johannesson@drakryggen.com>
2021-11-11 07:59:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var greeting = "<p>Hejsan hoppsan</p>";
+
+var firstname = "Christian";
+
+//alert(greeting);
+console.log(greeting);
+
+//I'm a one line comment
+
+/*
+I'm
+a
+multi
+line comment
+*/
+
+document.getElementById("text-container").innerHTML = greeting;
+
+document.querySelector(".name-container").innerHTML = firstname;
